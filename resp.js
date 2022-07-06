5a744633ecd28108f28234dbaa488930e343e115
Utkarsh Goyal <utkarsh2010202@akgec.ac.in>
2022-07-06 22:02:37 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,9 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navlist=document.querySelector('.navlist')
+rightnav=document.querySelector('.rightnav')
+burger.addeventlistener('click',()=>{
+    rightnav.classlist.toggle('vclass-resp')
+    navlist.classlist.toggle('vclass-resp')
+    navbar.classlist.toggle('hnav-resp')
+})
